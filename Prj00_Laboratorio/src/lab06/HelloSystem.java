393ef11125603354941905367c0f73e5281daaf3
MatteoGamba1993 <97434603+MatteoGamba1993@users.noreply.github.com>
2022-01-25 11:56:04 +0100

    Add files via upload

---
+++ Prj00_Laboratorio/src/lab06/HelloSystem.java
@@ -0,0 +1,28 @@
+package lab06;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class HelloSystem {
+
+	public static void main(String[] args) {
+
+		String property = System.getProperty("java.vendor");
+		System.out.println(property);
+		
+		Properties properties = System.getProperties();
+		
+		properties.list(System.out);
+		
+		System.gc();
+		
+		try {
+			Runtime.getRuntime().exec("sol");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ Prj00_Laboratorio/src/lab06/HelloWrapper.java
@@ -0,0 +1,15 @@
+package lab06;
+
+public class HelloWrapper {
+
+	public static void main(String[] args) {
+
+		String eta = "22";
+		
+		int etaInt = Integer.parseInt(eta);
+		int max = Integer.max(7, 6);
+		System.out.println(max);
+		
+	}
+
+}
