d309cf75232247cf7fde723b5642385a0ef67a3e
MatteoGamba1993 <97434603+MatteoGamba1993@users.noreply.github.com>
2022-01-28 12:54:16 +0100

    Add files via upload

---
+++ Prj24_MagazzinoTechno/src/main/java/controller/MagazzinoCtrl.java
@@ -0,0 +1,56 @@
+package controller;
+
+import java.util.ArrayList;
+
+import model.Articolo;
+import model.Movimento;
+
+public class MagazzinoCtrl {
+
+	private Articoli ctrlArticoli;
+	private MovimentiCtrl ctrlMovimenti;
+	
+	public MagazzinoCtrl() {
+		this.ctrlArticoli = new Articoli();
+		this.ctrlMovimenti= new MovimentiCtrl(new ArrayList<>());
+		this.init();
+	}
+	
+	private void init() {
+		
+		for (Articolo a : db.Articoli.getAsList()) {
+			this.ctrlArticoli.addArticolo(a);
+		}
+		
+	}
+	
+	public Articoli getCtrlArticoli() {
+		return ctrlArticoli;
+	}
+	
+	public MovimentiCtrl getCtrlMovimenti() {
+		return ctrlMovimenti;
+	}
+	
+	public void caricaMagazzino(Articolo a, int q, String desc){
+		
+		//creo un oggetto di tipo movimento
+		Movimento m = new Movimento("Carico Magazzino " + desc, q, a); 
+		//aggiungo il movimento alla lista di movimenti del ctrl movimenti
+		this.ctrlMovimenti.addMovimento(m);
+		//modifico la quantità disponibile per l'articolo a
+		this.ctrlArticoli.caricaArticolo(a, q);
+	}
+	
+	public void scaricaMagazzino(Articolo a, int q, String desc){
+		
+		//creo un oggetto di tipo movimento
+		Movimento m = new Movimento("Scarico Magazzino " + desc, q, a); 
+		//modifico la quantità disponibile per l'articolo a
+		if(this.ctrlArticoli.scaricaArticolo(a, q)){
+			//aggiungo il movimento alla lista di movimenti del ctrl movimenti
+			this.ctrlMovimenti.addMovimento(m);			
+		}
+	}
+	
+}
---
+++ Prj24_MagazzinoTechno/src/main/webapp/menu.jsp
@@ -0,0 +1,4 @@
+<div id="menu">
+	<a href="articoli">Articoli</a>
+	<a href="movimenti">Movimenti</a>
+</div>
